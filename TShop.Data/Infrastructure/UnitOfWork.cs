213ad655e1a6be24d213d89683a7b26df868341f
nam.nguyen <nam.nguyen@websynergies.com>
2023-06-09 01:08:30 +0700

    base structure

---
+++ TShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TShop.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory _dbFactory;
+        private TShopDbContext dbContext;
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+        public TShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = _dbFactory.Init()); }
+        }
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+
+}
---
+++ TShop.Data/TShopDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShop.Model.Models;
+
+namespace TShop.Data
+{
+    public class TShopDbContext : DbContext
+    {
+        public TShopDbContext() : base("TShopConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+        public DbSet<Footer> Footers { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuGroup> MenuGroups { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostCategory> PostCategories { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Slide> Slides { get; set; }
+        public DbSet<SupportOnline> SupportOnlines { get; set; }
+        public DbSet<SystemConfig> SystemConfigs { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<VisitorStatistic> VisitorStatistics { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
